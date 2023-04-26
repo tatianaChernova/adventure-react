6b72bfbac1b723b487bfabc6557614bcb7cf60c5
Tatiana Chernova <fedoseeva.tv@gmail.com>
2023-04-26 14:38:03 +0300

    Adventure project

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+        <Footer />
+      </Router>
+
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/components/HeroSection.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+import Video from "../videos/video-1.mp4";
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <video src={Video} autoPlay loop muted />
+
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
+            <div class="hero-btns">
+                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+                    GET STARTED
+                </Button>
+                <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
+                    WATCH TRAILER <i className='fa fa-play-circle' />
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default HeroSection;
